0fd6a21a7c5104802c6f54752ee54c5ab8dfe6f6
kevin.tsou <kjkin2004@hotmail.com>
2020-02-28 19:05:46 +0800

    1. add sliding_panel_layout.xml/activity_main_new.xml layout files

--- app/src/main/java/com/kai/speed/MainActivity.java
+++ app/src/main/java/com/kai/speed/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_new);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Tool bar
         Toolbar toolbar = findViewById(R.id.toolbar);
